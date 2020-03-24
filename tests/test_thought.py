930f96249cb2e27210dc162f4f04f130479af7fa
Roy Peleg <roypeleg@mail.tau.ac.il>
2020-03-24 21:11:34 +0200

    Fixed assertion

--- tests/test_thought.py
+++ tests/test_thought.py
@@ -7,9 +7,9 @@ from BaMMI.thought import Thought
 
 
 user_id = 1
-datetime = dt.datetime(2000, 1, 1, 12, 0)
+datetime = dt.datetime(2000, 1, 1, 10, 0)
 thought = "I'm hungry"
-serialized = b"\x01\x00\x00\x00\x00\x00\x00\x00 \xd0m8\x00\x00\x00\x00\n\x00\x00\x00I'm hungry"
+serialized = b"\x01\x00\x00\x00\x00\x00\x00\x00@\xd0m8\x00\x00\x00\x00\n\x00\x00\x00I'm hungry"
 
 
 @pytest.fixture
