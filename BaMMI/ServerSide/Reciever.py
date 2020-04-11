0652d4667ad7b9ab6b2acf9328fefd2218f5fae1
roypel <31568986+roypel@users.noreply.github.com>
2020-04-11 12:12:08 +0300

    wip

--- BaMMI/ServerSide/Reciever.py
+++ BaMMI/ServerSide/Reciever.py
@@ -1,16 +1,19 @@
+import json
 from flask import Blueprint, jsonify, request, url_for
 from BaMMI.BaMMI_pb2 import Snapshot, User
+from google.protobuf.json_format import MessageToDict
 from BaMMI.ServerSide import Utils
 
 
 bp = Blueprint('recieve_data', __name__, url_prefix='/transfers')
-available_parsers_list = ['pose', 'color_image', 'feelings', 'depth_image', 'datetime']
+message_type_data = ['pose', 'feelings', 'datetime']
+binary_type_data = ['color_image', 'depth_image']
 known_users = {}
 
 
 @bp.route('/config', methods=['GET'])
 def send_server_supported_fields():
-    return jsonify(available_parsers_list)
+    return jsonify([*message_type_data, *binary_type_data])
 
 
 @bp.route('/users', methods=['POST'])
@@ -30,11 +33,18 @@ def receive_snapshot_data():
     snapshot_data = request.data
     snapshot = Snapshot()
     snapshot.ParseFromString(snapshot_data)
-    color_image_path = Utils.build_path_for_files_from_data('.', user_id, snapshot, 'color_image.bin')
-    Utils.save_data_to_file(snapshot.color_image.data,
-                            color_image_path,
-                            'b')
-    snapshot.color_image = color_image_path
+    data_to_publish = prepare_data_for_queue(user_id, snapshot)
     with open('./snapshots', 'a') as f:
-        print([known_users[user_id], snapshot], file=f)
+        print([known_users[user_id], data_to_publish], file=f)
     return jsonify(success=True)
+
+
+def prepare_data_for_queue(user_id, data):
+    # if data is dict:
+    #     if [field for field in data if field not in [*message_type_data, *binary_type_data]]:
+    #         raise ValueError("Got field that parsers can't handle")
+    #     data['user_data'] = known_users[user_id]
+    #     return json.dumps(data)
+    # if data is Snapshot:
+    data_to_publish = MessageToDict(data, preserving_proto_field_name=True)
+    # TODO: In case data comes in other options, convert it to a valid json
