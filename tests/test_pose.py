b3b4d39fa2e77a2148781e9aa1dd3724dde3ae3d
roypel <31568986+roypel@users.noreply.github.com>
2020-06-01 01:46:23 +0300

    sleeeeeppppp

--- tests/test_pose.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from BaMMI.parsers.ParserHandler import ParserHandler
-
-
-# Not a real test, used only to check docker-compose finished loading RMQ which takes most of the time
-def test_parsers_up():
-    ph = ParserHandler()
-    ph.run_parser('pose', 'rabbitmq://127.0.0.1:5672/')
