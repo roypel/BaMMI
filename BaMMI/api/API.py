37f25b41a31d22e89a1fc325e3fff6619f59b0f9
roypel <31568986+roypel@users.noreply.github.com>
2020-05-31 21:06:56 +0300

    API Draft

--- BaMMI/api/API.py
+++ BaMMI/api/API.py
@@ -16,17 +16,26 @@ def run_api_server(host='127.0.0.1', port=5000, database_url=mongodb_url):
     app.run(host=host, port=port)
 
 
-@bp.route('')
+@bp.route('', methods=['GET'])
 def get_all_users():
     return jsonify(db.query_data({}, {'_id': 0, 'user_id': 1, 'username': 1}))
 
 
-@bp.route('/<int:user_id>')
+@bp.route('/<user_id>', methods=['GET'])
 def get_user_data(user_id):
     return jsonify(db.query_data({'user_id': user_id},
                                  {'_id': 0, 'user_id': 1, 'username': 1, 'birthday': 1, 'gender': 1}))
 
 
-@bp.route('/<int:user_id>/snapshots')
+@bp.route('/<user_id>/snapshots', methods=['GET'])
 def get_user_snapshots(user_id):
     return jsonify(db.query_data({'user_id': user_id}, {'_id': 0, 'user_id': 0, 'snapshots.datetime': 1}))
+
+
+@bp.route('/<user_id>/snapshots/<snapshot_id>', methods=['GET'])
+def get_snapshot_details(user_id, snapshot_id):
+    snapshot_data = db.query_data({'user_id': user_id, 'snapshots.datetime': snapshot_id},
+                                  {'_id': 0, 'user_id': 0, 'birthday': 0, 'gender': 0, 'username': 0,
+                                   'snapshots.datetime': 0})
+    available_fields = snapshot_data['snapshots'].keys()
+    return jsonify(available_fields)
