522ff3292077069c433db2b686c3708eefc2942f
roypel <31568986+roypel@users.noreply.github.com>
2020-05-31 21:52:41 +0300

    Trying to fix Mongo

--- BaMMI/api/API.py
+++ BaMMI/api/API.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, send_from_directory
 from ..utils.APIServer import FlaskWrapper
-from ..utils.Constants import mongodb_url
+from ..utils.Constants import storage_folder, mongodb_url
 from ..utils.DBWrapper import DBWrapper
+from ..utils.UtilFunctions import build_path_for_files_from_data
 
 
 bp = Blueprint('serve_data', __name__, url_prefix='/users')
@@ -37,5 +38,25 @@ def get_snapshot_details(user_id, snapshot_id):
     snapshot_data = db.query_data({'user_id': user_id, 'snapshots.datetime': snapshot_id},
                                   {'_id': 0, 'user_id': 0, 'birthday': 0, 'gender': 0, 'username': 0,
                                    'snapshots.datetime': 0})
-    available_fields = snapshot_data['snapshots'].keys()
+    available_fields = list(snapshot_data['snapshots'].keys())
     return jsonify(available_fields)
+
+
+@bp.route('/<user_id>/snapshots/<snapshot_id>/<result_name>', methods=['GET'])
+def get_parsed_result(user_id, snapshot_id, result_name):
+    result_name = result_name.replace("-", "_")
+    snapshot_data = db.query_data({'user_id': user_id, 'snapshots.datetime': snapshot_id},
+                                  {'_id': 0, f'snapshots.{result_name}': 1})
+    result = snapshot_data['snapshots'][0][result_name]
+    if isinstance(result, str):  # TODO: Come on... You can do better than that...
+        possible_file_path = result.split(storage_folder)[1]
+        if possible_file_path:  # We found that we're about to return path to the file from our storage folder
+            return jsonify(f'GET /users/{user_id}/snapshots/{snapshot_id}/{result_name}/data')
+    return jsonify(result)
+
+
+@bp.route('/<user_id>/snapshots/<snapshot_id>/<result_name>/data', methods=['GET'])
+def get_file(user_id, snapshot_id, result_name):
+    result_name = result_name.replace("-", "_")
+    return send_from_directory(storage_folder, build_path_for_files_from_data('.', user_id, snapshot_id,
+                                                                              f'{result_name}.jpg'))
--- BaMMI/utils/Constants.py
+++ BaMMI/utils/Constants.py
@@ -1,4 +1,5 @@
 
+storage_folder = 'BaMMI/storage'
 
 mongodb_url = "mongodb://BaMMI:1337@mongo:27017"
 rabbit_mq_url = 'rabbitmq://rabbitmq:5672/'
