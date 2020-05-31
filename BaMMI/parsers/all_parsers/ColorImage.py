036436dff91666cb328b36bb6de178309749464a
roypel <31568986+roypel@users.noreply.github.com>
2020-05-31 18:18:52 +0300

    Removing debug prints (that causes bugs)

--- BaMMI/parsers/all_parsers/ColorImage.py
+++ BaMMI/parsers/all_parsers/ColorImage.py
@@ -7,9 +7,6 @@ def parse_color_image(context, snapshot):
     save_path = context.generate_path('color_image.jpg')
     size = snapshot['color_image']['width'], snapshot['color_image']['height']
     image_data_path = snapshot['color_image']['data']
-    import os
-    print(f"WE're HERE: {os.getcwd()} we see: {os.listdir('/usr/src/BaMMI')} and above us: {os.listdir('..')}")
-    print(f"DATA IS HERE: {os.listdir(os.path.dirname(f'{image_data_path}/../../..'))}")
     with open(image_data_path, 'rb') as f:
         image_data = f.read()
     image = PILIm.new('RGB', size)
